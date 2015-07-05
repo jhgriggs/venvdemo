c5ccb2f9fb82165898adf858e5a2299fa222f35b
Janette Griggs <jhgriggs.github@gmail.com>
2015-07-05 08:28:12 -0700

    First commit of project

---
+++ demoscript.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def main():
+    message = "I have successfully created a Git repo!"
+    print message
+    message = "I also created a virtualenv that should not be pushed " + \
+              "to the GitHub repo."
+    print message
+
+if __name__ == "__main__": 
+    main()
